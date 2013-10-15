123d34b0f07d34c0e6eae1809c749fa36ff6756c
Joseph Rogina <joe.rogina@carmelsoft.com>
2013-10-15 10:36:11 -0700

    Initial Commit

---
+++ Hello World 2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hello World 2
+//
+//  Created by Carmel on 10/15/13.
+//  Copyright (c) 2013 Carmel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hello World 2Tests/Hello_World_2Tests.m
@@ -0,0 +1,34 @@
+//
+//  Hello_World_2Tests.m
+//  Hello World 2Tests
+//
+//  Created by Carmel on 10/15/13.
+//  Copyright (c) 2013 Carmel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Hello_World_2Tests : XCTestCase
+
+@end
+
+@implementation Hello_World_2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
