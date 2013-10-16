207ec7d266af7a71e8ad7c36cc891e53a06ac9db
CarmelMike <miketstrickland@gmail.com>
2013-10-16 13:48:56 -0700

    Added Parent Class, the keyboard is buggy though

---
+++ Hello World 2/HelloParentViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloParentViewController.h
+//  Hello World 2
+//
+//  Created by Carmel on 10/16/13.
+//  Copyright (c) 2013 Carmel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloParentViewController : UIViewController
+
+@end
---
+++ Hello World 2/HelloParentViewController.m
@@ -0,0 +1,38 @@
+//
+//  HelloParentViewController.m
+//  Hello World 2
+//
+//  Created by Carmel on 10/16/13.
+//  Copyright (c) 2013 Carmel. All rights reserved.
+//
+
+#import "HelloParentViewController.h"
+
+@interface HelloParentViewController ()
+
+@end
+
+@implementation HelloParentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
