8cd77c5ce69ae787d04e41031720b5822496d79d
CarmelMike <miketstrickland@gmail.com>
2013-10-16 16:42:34 -0700

    After Slider change. In process of commenting code, etc.

--- Hello World 2/HelloViewController.m
+++ Hello World 2/HelloViewController.m
@@ -19,14 +19,14 @@
 
 - (id)init
 {
-    self.startingYCoordinate = 60;
-    self.numberOfButtons = 100;
-    self.sliderValue = 2;
-    self.fullNumber = @"";
+    self.startingYCoordinate = 60; //sets the starting Y coordinates for the text field, slider, and buttons
+    self.numberOfButtons = 2;      //sets the initial number of buttons displayed on the screen to 2
+    self.sliderValue = 2;          //sets default slider value to 2
+    self.fullNumber = @"";         //fullNumber is used with the textField delegate. The delegate will create a string of the numbers inputted to the text box and convert them to an int
     NSLog(@"start init: %i", self.numberOfButtons);
     self = [super init];
     if (self) {
-        self.buttonArray = [self createButtonArrayWithSize:self.numberOfButtons];
+        self.buttonArray = [self createButtonArrayWithSize:5];
         self.numberField = [self createTextField:30];
         self.sliderBar = [self createSlider:80];
     }
@@ -34,25 +34,45 @@
     return self;
 }
 
+
+//Creates sliders bar. Make also be created dynamically
+
 - (UISlider*) createSlider:(NSInteger) yOrigin
 {
     CGRect frame = CGRectMake(50, self.startingYCoordinate + yOrigin, 220.0, 10.0);
     self.sliderBar = [[UISlider alloc] initWithFrame:frame];
-//    [slider addTarget:self action:@selector(sliderAction:) forControlEvents:UIControlEventValueChanged];
     [self.sliderBar setBackgroundColor:[UIColor clearColor]];
     self.sliderBar.minimumValue = 0.0;
     self.sliderBar.maximumValue = 4.0;
-    self.sliderBar.continuous = NO;
     self.sliderBar.value = 2;
+    self.sliderBar.continuous = YES;
+    [self.sliderBar addTarget:self
+                       action:@selector(valueChanged:) //calls valueChanged function below
+     forControlEvents:UIControlEventValueChanged];
     self.sliderValue = self.sliderBar.value;
     [self.view addSubview:self.sliderBar];
     
     return self.sliderBar;
 }
 
+
+//Function designed to change the number of buttons that appear on the screen in realtime as the user moves the slider.
+
+-(void) valueChanged: (NSInteger) currentValueOfSilder
+{
+    
+    self.numberOfButtons = self.sliderBar.value;
+    [self clearButtonArray];
+    [self setValuesForButtons];
+    
+}
+
+
+//Method that is responsible for creating a text field. Can be dynamically used for multiple text fields.
+
 - (UITextField*) createTextField:(NSInteger) yOrigin
 {
-  //  UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(50, yOrigin, 220, 40)];
+
     self.numberField = [[UITextField alloc] initWithFrame:CGRectMake(50, self.startingYCoordinate + yOrigin, 220, 40)];
     self.numberField.borderStyle = UITextBorderStyleRoundedRect;
     self.numberField.font = [UIFont systemFontOfSize:15];
@@ -62,33 +82,22 @@
     self.numberField.returnKeyType = UIReturnKeyDone;
     self.numberField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
     self.numberField.delegate = self;
-    
-    //self.numberOfButtons = [self.numberField.text integerValue];
     NSLog(@"createTextField: %i", self.numberOfButtons);
     [self.view addSubview:self.numberField];
-    // [textField release];
     return self.numberField;
 }
     
 
+//Handles the closing on the keyboard when the user touches/clicks the screen.
+
 -(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
-    
-    self.numberOfButtons = self.sliderBar.value;
-    [self.sliderBar resignFirstResponder];
-    //self.numberOfButtons = [self.numberField.text integerValue];
+
     [self.numberField resignFirstResponder];
-    [self clearButtonArray];
-    [self setValuesForButtons];
-   // self.buttonArray = [self createButtonArrayWithSize:self.numberOfButtons];
-    //NSString *digit = [[sender titlelabel] text];
+    [self.sliderBar resignFirstResponder];
     NSLog(@"in touchesEnded: %i", self.numberOfButtons);
-//    NSLog(@%@", digit);
 
 }
-
-
-
 
 - (IBAction)backgroundTap:(id)sender {
 
@@ -132,8 +141,8 @@
 
 - (void)setValuesForButtons
 {
-    if (self.numberOfButtons >= 100){
-        self.numberOfButtons = 99;
+    if (self.numberOfButtons >= 5){
+        self.numberOfButtons = 5;
     }
     if (self.numberOfButtons <= 0){
         self.numberOfButtons = 0;
@@ -143,19 +152,6 @@
     for (int i = 0; i < self.numberOfButtons; i++) {
         int y = (i + 1) * 100 + yAdjustment;
         self.buttonArray[i] = [self setButton:self.buttonArray[i] WithFrame:CGRectMake(50, y, 220, 75) color:[UIColor redColor] title:@"Paint It Black"];
-
-        //saved if I want to go back to custom buttons with different text, etc
-        /*
-        switch (i) {
-            case 0:
-                self.buttonArray[0] = [self setButton:self.buttonArray[0] WithFrame:CGRectMake(40, 100, 250, 150) color:[UIColor redColor] title:@"Paint It Black"];
-                break;
-            case 1:
-                self.buttonArray[1] = [self setButton:self.buttonArray[1] WithFrame:CGRectMake(40, 300, 250, 150) color:[UIColor redColor] title:@"Paint It Black"];
-            default:
-                break;
-        }
-         */
     }
 }
 
@@ -185,61 +181,13 @@
     else if (tempButton.backgroundColor == [UIColor redColor]){
         tempButton.backgroundColor = [UIColor blackColor];
     }
-
-    /*
-    switch (button.tag) {
-   
-   
-        case 0:
-            tempButton = (UIButton*)[self.buttonArray objectAtIndex:0];
-            if (tempButton.backgroundColor == [UIColor blackColor]){
-                tempButton.backgroundColor = [UIColor redColor];
-            }
-            else if (tempButton.backgroundColor == [UIColor redColor]){
-                tempButton.backgroundColor = [UIColor blackColor];
-            }
-            break;
-        case 1:
-            tempButton = (UIButton*)[self.buttonArray objectAtIndex:1];
-            if (tempButton.backgroundColor == [UIColor blackColor]){
-                tempButton.backgroundColor = [UIColor redColor];
-            }
-            else if (tempButton.backgroundColor == [UIColor redColor]){
-                tempButton.backgroundColor = [UIColor blackColor];
-            }
-            
-            
-        default:
-            break;
-    }
-*/
 }
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
 {
-   // NSLog(@"Length: %lu ",(unsigned long)range.length);
-   // NSLog(@"Range: %lu ",(unsigned long)range.location);
-   // NSLog(@"String: %@",string);
-    
-    
-    //NSString *errorTag = @"Error: ";
-    //NSString *errorString = @"premature end of file.";
-    //NSString *errorMessage = [errorTag stringByAppendingString:errorString];
-    
-//    - (NSString *)stringByReplacingCharactersInRange:(NSRange)range withString:(NSString *)replacement
 
     self.fullNumber = [self.fullNumber stringByReplacingCharactersInRange:range withString:string];
     
-    
-    //self.fullNumber = [self.fullNumber stringByAppendingString:string];
-    
-    //long len = string.length;
-  //  len = 2;
-    
-    /*
-     char c = [string characterAtIndex:(range.length)];
-    c = 'c';
-    */
     self.numberOfButtons = [self.fullNumber integerValue];
     //[self.numberField resignFirstResponder];
     [self clearButtonArray];
